5f14dd65b508715fe1666184982dd24a7cd34aa1
iksiks30 <iksiks30@gmail.com>
2023-01-15 20:04:33 +0300

    Добавьте файлы проекта.

---
+++ HomeWork15(new)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork15_new_
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(new TextForConsole().PrintText));
+                Thread.Sleep(500);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork15(new)/TextForConsole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork15_new_
+{
+    internal class TextForConsole
+    {
+        private Random random = new Random();
+        public void PrintText(object o)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"ID текущего потока {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(random.Next(2000,4000));
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Конец работы потока с ID {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
